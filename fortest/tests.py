7454569939fa48f1bba731dca033549318ecbd2f
farrar142 <gksdjf1690@gmail.com>
2021-12-29 20:27:09 +0900

    Asd

--- fortest/tests.py
+++ fortest/tests.py
@@ -11,13 +11,13 @@ class YourTestClass(testcases):
         print("setUp: Run once for every test method to setup clean data.")
         pass
 
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+    # def test_false_is_false(self):
+    #     print("Method: test_false_is_false.")
+    #     self.assertFalse(False)
 
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
+    # def test_false_is_true(self):
+    #     print("Method: test_false_is_true.")
+    #     self.assertTrue(False)
 
     def test_one_plus_one_equals_two(self):
         print("Method: test_one_plus_one_equals_two.")
