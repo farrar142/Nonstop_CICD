1007d4ffebd801f154737518a9a680d4d087e5e0
farrar142 <gksdjf1690@gmail.com>
2021-12-29 11:20:55 +0900

    asd

--- test.py
+++ test.py
@@ -44,9 +44,9 @@ def get_docker_containers():
                 status = words[4].strip().split(" ")[0]
                 # print(f"C Name :: {words[-1]}, C ID :: {words[0]}, Img Name :: {words[1]} , Status :: {status}")
                 _result = [words[-1], words[0], words[1], status]
-                # 실행중이면 ports값도 얻어내옴
+                # get ports
                 _result.append(get_ports_from_strings(_result, words))
-                # 인스턴스 ip 주소값도 알아내옴.
+                # get ip
                 _result.append(get_logs(
                     "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "+words[0]).strip("'"))
             except:
@@ -100,33 +100,22 @@ if __name__ == "__main__":
     try:
         now_con = Container(get_specific_container("now_con"))
     except:
-        print("현재 컨테이너가 존재하지 않아요.")
+        print("No Current Container.")
         init = True
-    # 이미지가 없을시 풀
     os.system("docker pull python:3")
-    # 새 이미지 생성
     os.system(f"docker build -t python:{now} .")
-    #테스트 컨테이너 8001번 생성
     os.system(f"docker run -d -p 8001:8001 --name test_con python:{now} gunicorn --bind 0:8001 base.wsgi")
     test_con = Container(get_specific_container("test_con"))
-    #테스트
     if connection_checker(test_con) == False:
-        #새 컨테이너 끄기
         os.system(f"docker rm -f test_con")
-        #새 이미지 삭제
         os.system(f"docker rmi -f python:{now}")
         sys.stderr.write("Build Failed")
         exit()
     else:
-        #elif test success
-        #새 컨테이너 끄기
         os.system(f"docker rm -f test_con")
-        #기존 컨테이너 끄기
         if init == False:
             os.system(f"docker rm -f now_con")
-        #새 이미지를 기존 컨테이너 이름으로 실행 8000포트로
         os.system(f"docker run -d -p 8000:8000 --name now_con python:{now} gunicorn --bind 0:8000 base.wsgi")
-        #오래된 이미지 삭제
         if init == False:
             os.system(f"docker rmi -f {now_con.image_name}")
         #성공 메세지.
