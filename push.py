96de6fa2a4447d5315e7e0e3cc45d2e5b76c8dce
farrar142 <gksdjf1690@gmail.com>
2021-12-30 17:21:25 +0900

    test

--- push.py
+++ push.py
@@ -1,8 +1,8 @@
 import os
-
+from deploy import get_now
 if __name__ == "__main__":    
     f = open("newfile.txt",'w')
-    f.write(f"test1")
+    f.write(f"test{get_now()}")
     f.close()
     os.system(f"git add . && git commit -m \"test\" && git push origin master")
 else:
