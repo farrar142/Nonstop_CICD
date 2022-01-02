13cfc477a22cc47120c2069ccdf1629a4e26f38a
-g <gksdjf1690@gmail.com>
2022-01-02 16:42:13 +0900

    test

--- push.py
+++ push.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     f = open("newfile.txt",'w')
     f.write(f"test{get_now()}")
     f.close()
-    os.system(f"git config --global  user.name farrar142")
-    os.system(f"git config --global  user.email gksdjf1690@gmail.com")
+    os.system(f"git config --unset--global user.name")
+    os.system(f"git config --unset--global user.name")
+    os.system(f"git config --global user.email gksdjf1690@gmail.com")
     os.system(f"git add . && git commit -m \"test\" && git push origin master")
 else:
     for i in range(20):
