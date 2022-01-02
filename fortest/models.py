382049ed865b7402b73cf963545a6b810da752a3
Kimbangul <highcolor_12@naver.com>
2022-01-02 15:58:30 +0900

    test

--- fortest/models.py
+++ fortest/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
+    gender = models.CharField('성별', max_length=1, blank=True, choices=GenderChoices.choices)
+    avatar = models.ImageField('아바타', blank=True, upload_to="accounts/avatar/%Y/%m/%d", help_text="100px * 100px 크기의 gif/png/jpg 이미지를 업로드해주세요.")
+
+
+class Market(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    name = models.CharField('마켓이름', max_length=100)
+    site_url = models.URLField('마켓사이트URL', max_length=100)
+    email = models.EmailField('마켓대표이메일', max_length=100)
+    master = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Product(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    is_deleted = models.BooleanField('삭제여부', default=False)
+    delete_date = models.DateTimeField('삭제날짜', null=True, blank=True)
+    market = models.ForeignKey(Market, on_delete=models.DO_NOTHING)
+    image = models.ImageField(upload_to='products/images/',null=True)
+    name = models.CharField('상품명(내부용)', max_length=100)
+    display_name = models.CharField('상품명(고객용)', max_length=100)
+    price = models.PositiveIntegerField('권장판매가')
+    sale_price = models.PositiveIntegerField('실제판매가')
+    is_hidden = models.BooleanField('노출여부', default=False)
+    is_sold_out = models.BooleanField('품절여부', default=False)
+    category_id = models.PositiveIntegerField('카테고리번호(추후설계)', default=0)
+    hit_count = models.PositiveIntegerField('조회수', default=0)
+    review_count = models.PositiveIntegerField('리뷰수', default=0)
+    review_point = models.PositiveIntegerField('리뷰평점', default=0)
+
+    def colors(self):
+        colors = []
+        product_reals = self.product_reals.all()
+        for product_real in product_reals:
+            colors.append(product_real.option_2_name)
+        html = ''
+        for color in colors:
+            html += "<span></span>"
+
+    def __str__(self):
+        return self.display_name
+
+
+class ProductReal(models.Model):
+    reg_date = models.DateTimeField('등록날짜', auto_now_add=True)
+    update_date = models.DateTimeField('갱신날짜', auto_now=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    option_1_type = models.CharField('옵션1 타입', max_length=10, default='SIZE')
+    option_1_name = models.CharField('옵션1 이름(내부용)', max_length=50)
+    option_1_display_name = models.CharField('옵션1 이름(고객용)', max_length=50)
+    option_2_type = models.CharField('옵션2 타입', max_length=10, default='COLOR')
+    option_2_name = models.CharField('옵션2 이름(내부용)', max_length=50)
+    option_2_display_name = models.CharField('옵션2 이름(고객용)', max_length=50)
+    option_3_type = models.CharField('옵션3 타입', max_length=10, default='', blank=True)
+    option_3_name = models.CharField('옵션3 이름(내부용)', max_length=50, default='', blank=True)
+    option_3_display_name = models.CharField('옵션3 이름(고객용)', max_length=50, default='', blank=True)
+    is_sold_out = models.BooleanField('품절여부', default=False)
+    is_hidden = models.BooleanField('노출여부', default=False)
+    add_price = models.IntegerField('추가가격', default=0)
+    stock_quantity = models.PositiveIntegerField('재고개수, 품절일때 유용함', default=0)
+    def __str__(self):
+        return f"{self.product}상품 사이즈 : {self.option_1_display_name} 컬러 : {self.option_2_display_name} 추가됨"
--- fortest/tests.py
+++ fortest/tests.py
@@ -1,7 +1,45 @@
 from django.test import TestCase
+from .models import *
 
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #UserDataCreate
+        for id in range(1, 4):
+            username = f"user{id}"
+            password = f"user{id}"
+            first_name = f"이름{id}"
+            last_name = f"성{id}"
+            email = f"test{id}@test.com"
+            gender = 'M'
+            User.objects.create(username=username, password=password, first_name=first_name, last_name=last_name,email=email, gender=gender)
+        #MarketDataCreate
+        Market(name="언니네옷가게", site_url="https://www.abc1.co.kr", email="test1@test.com", master_id=1).save()
+        Market(name="누나네옷가게", site_url="https://www.abc2.co.kr", email="test2@test.com", master_id=2).save()
+        Market(name="이모네옷가게", site_url="https://www.abc3.co.kr", email="test3@test.com", master_id=3).save()
+        #ProductDataCreate
+        product = Product(market_id=1, name="원피스1", display_name="인스타여신원피스1", price=10000, sale_price=9000)
+        product.save()
+        #ProductRealDataCreate
+        ProductReal(product=product, option_1_name="44", option_1_display_name="44", option_2_name="RED",
+                option_2_display_name="감성레드").save()
 
+    def test_First_name_label(self):
+        author = User.objects.get(id=3)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label,'이름')#ko-kr = '이름' en-us = 'first_name'
 
-class test1(TestCase):
-    def testExample(self):
-        self.assertEqual(1+1, 2)
+    def test_Market_name_label(self):
+        market = Market.objects.get(id=3)
+        field_label = market._meta.get_field('site_url').verbose_name
+        self.assertEquals(field_label,'마켓사이트URL')
+
+    def test_Product_Type_Test(self):
+        product = Product.objects.get(id=1)
+        price = product.price
+        self.assertEquals(type(price),int)
+        
+    def test_Product_Real_Type_Test(self):
+        productreal = ProductReal.objects.get(id=1)
+        name = productreal.option_1_display_name
+        self.assertEquals(type(name),str)
